395da743cec41fe15ce397048d39734c5a6a780d
Pantoji <pantojis@emea.travelex.net>
2018-04-10 12:08:06 +0530

    Changes for Default Config Repository

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-module.exports.printSomething = function()
+var log4js = require(log4js);
+var defaultConfig = require('./config/log4js')
+function getLogger(loggerName) {
+	return log4js.getLogger(loggerName);
+}
+
+function initLogger() {
+	try {
+		require('fs').mkdirSync('./log');
+	} catch (e) {
+		if (e.code != 'EEXIST') {
+			console.error("Could not set up log directory, error was: ", e);
+			process.exit(1);
+		}
+	}
+}
+
+function configureLogger(config)
 {
-	console.log("Something is here ");
+	log4js.configure(config);
+}
+
+function configureLogger()
+{
+	configureLogger(defaultConfig);
+}
+
+module.exports.travelex_logger = {
+	getLogger,initLogger,configureLogger
 }
